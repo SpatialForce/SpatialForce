8d68718b6514b4c9dc47061bcb82958ce84ed27a
yangfengzzz <yangfengzzz@hotmail.com>
2024-02-22 19:57:33 +0800

    add runtime buffer

--- runtime/buffer.h
+++ runtime/buffer.h
@@ -6,33 +6,78 @@
 
 #pragma once
 
-#include <vector>
 #include "device.h"
 
 namespace vox {
 template<typename T>
 class Buffer {
 public:
-    explicit Buffer(const Device &device) : _device{device} {
+    Buffer(const Device &device, size_t n) : _device{device}, _n{n} {
         ContextGuard guard(device.primary_context());
-        check_cuda(cudaMalloc(&_device_buffer, sizeof(T)));
+        _byte_size = sizeof(T) * n;
+        check_cuda(cudaMalloc(&_handle, _byte_size));
     }
 
     ~Buffer() {
         ContextGuard guard(_device.primary_context());
-        check_cuda(cudaFree(_device_buffer));
+        check_cuda(cudaFree(_handle));
+    }
+
+    inline T *handle() {
+        return _handle;
+    }
+
+    [[nodiscard]] inline size_t byte_size() const {
+        return _byte_size;
+    }
+
+    [[nodiscard]] inline size_t size() const {
+        return _n;
+    }
+
+    [[nodiscard]] inline const Device &device() const {
+        return _device;
     }
 
 private:
+    size_t _n;
+    size_t _byte_size;
     const Device &_device;
-    std::vector<T> _host_buffer;
-    void *_device_buffer{};
+    T *_handle{};
 };
 
 template<typename T>
-Buffer<T> create_buffer(uint32_t index = 0) {
+Buffer<T> create_buffer(size_t n, uint32_t index = 0) {
     const auto &d = device(index);
-    return Buffer<T>{d};
+    return {d, n};
+}
+
+template<typename T>
+void sync_h2d(void *src, Buffer<T> &dst) {
+    auto &device = dst.device();
+    ContextGuard guard(device.primary_context());
+    check_cuda(cudaMemcpyAsync(dst.handle(), src, dst.byte_size(), cudaMemcpyHostToDevice, device.stream.handle()));
+}
+
+template<typename T>
+void sync_d2h(Buffer<T> &src, void *dst) {
+    auto &device = src.device();
+    ContextGuard guard(device.primary_context());
+    check_cuda(cudaMemcpyAsync(dst, src.handle(), src.byte_size(), cudaMemcpyDeviceToHost, device.stream.handle()));
+}
+
+template<typename T>
+void sync_d2d(const Buffer<T> &src, Buffer<T> &dst) {
+    auto &device = src.device();
+    ContextGuard guard(device.primary_context());
+    check_cuda(cudaMemcpyAsync(dst.handle(), src.handle(), src.byte_size(), cudaMemcpyDeviceToDevice, device.stream.handle()));
+}
+
+template<typename T>
+void memset(Buffer<T> &dst, int value) {
+    auto &device = dst.device();
+    ContextGuard guard(device.primary_context());
+    check_cuda(cudaMemsetAsync(dst, value, dst.size(), device.stream.handle()));
 }
 
 }// namespace vox
--- tests/runtime/buffer_tests.cu
+++ tests/runtime/buffer_tests.cu
@@ -12,5 +12,10 @@ using namespace vox;
 TEST(Buffer, constructor) {
     vox::init();
 
-    auto buffer = create_buffer<float>();
+    auto buffer = create_buffer<float>(10);
+    std::vector<float> a{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    sync_h2d(a.data(), buffer);
+    std::vector<float> result(10);
+    sync_d2h(buffer, result.data());
+    EXPECT_EQ(result[0], 1);
 }
